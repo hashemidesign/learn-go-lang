0e8340f2c05da60cdce5c0543fa1ba6a7a8f6777
Mohsen Hashemi <m.hashemi.code@gmail.com>
2021-12-17 05:46:42 +0330

    make eliza works

--- 02-eliza/main.go
+++ 02-eliza/main.go
@@ -1,11 +1,28 @@
 package main
 
 import (
+	"bufio"
 	"elizaapp/doctor"
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
+	reader := bufio.NewReader(os.Stdin)
 	var greet string = doctor.Intro()
 	fmt.Println(greet)
+
+	for {
+		fmt.Print("-> ")
+		userInput, _ := reader.ReadString('\n')
+		// check if user the input is "quit" --> we must remove \n (enter key)
+		userInput = strings.Replace(userInput, "\r\n", "", -1) // for windows
+		userInput = strings.Replace(userInput, "\n", "", -1)   // for linux and mac
+		if userInput == "quit" {
+			break
+		} else {
+			fmt.Println(doctor.Response(userInput))
+		}
+	}
 }
