0111872060465a1771e3b437ffb0a9537a23e83d
Mohsen Hashemi <m.hashemi.code@gmail.com>
2021-12-17 04:58:31 +0330

    how to use function and variable

--- 01-hello-world/main.go
+++ 01-hello-world/main.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, world")
+	// var greet string = "Hello, world"
+	greet := "Hello, world" // short-hand
+	shout(greet)
+}
+
+func shout(sentence string) {
+	fmt.Println(sentence)
 }
