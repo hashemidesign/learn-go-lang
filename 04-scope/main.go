84fb09b7499cd0f2ca453c95d6b53f04e49294ed
Mohsen Hashemi <m.hashemi.code@gmail.com>
2021-12-18 04:50:48 +0330

    package and block scope

--- 04-scope/main.go
+++ 04-scope/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import "fmt"
 
+var one = 1 // package level varaible
+
+func main() {
+	var one = "one" // block level varaible
+
+	fmt.Println(one)
+	myFunc()
+
+	// block level variables has priority
+	// and is a bad pactice to have shadowing variables in a program
+}
+
+func myFunc() {
+	// var one = "the number one"
+	fmt.Println(one)
 }
